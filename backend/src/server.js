6639a684111865928305326dce06211bdf46f460
Ayden Hawkins <aydenhawkins@Mac.lan>
2025-09-26 00:03:42 -0400

    Added import endpoint to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ const playerRouter = require('./routes/player');
 const matchRouter = require('./routes/match');
 const playerStatsRouter = require('./routes/playerStats');
 const valorantRouter = require('./routes/valorant');
+const matchImportRouter = require('./routes/matchImport');
 
 const app = express();
 app.use(cors({
@@ -20,6 +21,7 @@ app.use('/players', playerRouter);
 app.use('/matches', matchRouter);
 app.use('/playerstats', playerStatsRouter);
 app.use('/valorant', valorantRouter);
+app.use('/import', matchImportRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
