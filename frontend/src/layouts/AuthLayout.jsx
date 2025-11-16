9228971e2abb08bc12715c32b92e3158db7c3b3f
Ayden Hawkins <aydenhawkins@Mac.lan>
2025-11-15 23:43:56 -0500

    Minor styling fixes

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-[#2C0F74]">
-            <Outlet />
+        <div className="min-h-screen flex items-center justify-center bg-[#2C0F74] px-4">
+            <div className="w-full max-w-lg">
+                <Outlet />
+            </div>
         </div>
     );
 }
